de02a96e884235d89358431c7adfd4861126a9fe
Millie Chan <mireechan@gmail.com>
2016-09-05 15:29:02 -0700

    - Create new cocoa class file ViewController2 and add buttons and labels to Initial View in Storyboard

---
+++ Huntr/ViewController2.h
@@ -0,0 +1,14 @@
+//
+//  ViewController2.h
+//  Huntr
+//
+//  Created by Millie Chan on 9/5/16.
+//  Copyright © 2016 Edward Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *gameLabel;
+@property (weak, nonatomic) IBOutlet UIButton *gameButton;
+@end
