3a2ced0528f5ea91a9342e4c8ef0d90a38747e3f
Millie Chan <mireechan@gmail.com>
2016-09-06 10:41:19 -0700

    Update Initial Clue View Controller in Storyboard and connect to AR view

---
+++ Huntr/LandingViewController.h
@@ -0,0 +1,14 @@
+//
+//  LandingViewController.h
+//  Huntr
+//
+//  Created by Millie Chan on 9/5/16.
+//  Copyright © 2016 Edward Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *gameButton;
+@end
