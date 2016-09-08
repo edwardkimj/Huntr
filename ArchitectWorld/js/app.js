e87e9cde4d94e35581cc6c2dd92235ced9cf29e7
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-09-08 11:18:40 -0700

    Pass game url to selectingpois.js and add logo

--- ArchitectWorld/js/app.js
+++ ArchitectWorld/js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
   $("#panel-poidetail").panel("open", 123);
 
-  console.log("I'm in the app.js file");
   $.ajaxSetup ({
     cache: false
   });
@@ -12,9 +11,11 @@ $(document).ready(function() {
       $('#map-list').empty();
       for( var i = 0; i < maps.length; i++ ) {
         console.log("I'm in the for loop for getting maps");
-        $('#map-list').append(`<li><a  href="${maps[i].id}">${maps[i].name}</a></li>`);
-        listenForClick();
+        $('#map-list').append(`<li><a href="#" data-id="${maps[i].id}">${maps[i].name}</a></li>`);
       }
+      listenForClick();
+      $('.hunts').show();
+      $('.hunt-detail').hide();
     }
     $.ajax({
       url: "https://protected-anchorage-46542.herokuapp.com/users/2/games/",
@@ -36,50 +37,22 @@ $(document).ready(function() {
     });
   }
 
-
-  function setView() {
-    console.log("I'm in setView!!!")
-    var hashParts = document.location.hash.split('/');
-    if (hashParts[0] === '') {
-      console.log("i'm in the setView and about to load the maplist page")
-      loadMapListPage();
-    } else {
-      console.log("i'm in setview and i'm trying to go to details")
-      loadMapDetailPage(hashParts[1]);
-    }
-  }
-
-  $('#map-list a').on('tap', function(event) {
-      event.preventDefault();
-      // grab href with the maps id
-      // clear out the html page and inject details of maps
-      console.log("clicked on map-quest link")
-  })
-
-  setView();
-
-  window.addEventListener('hashchange', setView);
+  loadMapListPage();
 });
 
 
 function listenForClick() {
-  $('#map-list').on('click', 'a', function(event) {
+  console.log('listenForClick');
+  var test = $('#map-list').on('click', 'a', function(event) {
+    console.log('listenForClick CLICKED');
     event.preventDefault();
-    var mapLink = this;
-    var mapNumber = $(mapLink).attr('href');
-    var link = "https://protected-anchorage-46542.herokuapp.com/users/2/games/" + mapNumber + "/steps"
-
-    console.log(link)
-
-    // var mapDetailsRequest = $.ajax({
-    //   url: ''
-    // })
-
-    // grab href with the maps id
-    // clear out the html page and inject details of maps
-    console.log("clicked on map-quest link")
+    console.log('CURRENT MAP ID');
+    console.log($(this).data('id'));
+    $('#current-map').data('id', $(this).data('id'));
+    $('.hunts').hide();
+    $('.hunt-detail').show();
+    $("#panel-poidetail").panel("close");
+    $.getScript("js/selectingpois.js");
+    $.getScript("js/marker.js");
   });
 };
-
-
-//if (hashParts[0] === '#maps' && hashParts[1]) {
