043ec91ca14e9cccabeecaecf9f86ab37c57bdf4
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-09-08 11:55:23 -0700

    Refactor sending map info to selectingpois.js

--- ArchitectWorld/js/selectingpois.js
+++ ArchitectWorld/js/selectingpois.js
@@ -13,10 +13,72 @@ var ServerInformation = {
     POIDATA_SERVER_ARG_NR_POIS: "nrPois"
 };
 
-console.log(ServerInformation);
+var SockWorld = {
+    loaded: false,
+    rotating: false,
+
+    init: function initFn() {
+        this.createModelAtLocation();
+    },
+
+    createModelAtLocation: function createModelAtLocationFn() {
+
+    /*
+     First a location where the model should be displayed will be defined. This location will be relativ to the user.
+     */
+        var location = new AR.RelativeLocation(null, 5, 0, 2);
+
+    /*
+     Next the model object is loaded.
+     */
+        var modelEarth = new AR.Model("assets/huntr-dirtysock.wt3", {
+                                  onLoaded: this.worldLoaded,
+                                  scale: {
+                                  x: 2,
+                                  y: 2,
+                                  z: 2
+                                  },
+                                  rotate: {
+                                  roll: 180.0,
+                                  tilt: 0.0,
+                                  heading: 0.0
+                                  },
+
+                                  translate: {
+                                  x: 5,
+                                  y: 5,
+                                  z: 0
+                                  }
+                                  });
+
+        var indicatorImage = new AR.ImageResource("assets/indi.png");
+
+        var indicatorDrawable = new AR.ImageDrawable(indicatorImage, 0.1, {
+                                                 verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+                                                 });
+
+    /*
+     Putting it all together the location and 3D model is added to an AR.GeoObject.
+     */
+        var obj = new AR.GeoObject(location, {
+                               drawables: {
+                               cam: [modelEarth],
+                               indicator: [indicatorDrawable]
+                               }
+                               });
+    },
+
+    worldLoaded: function worldLoadedFn() {
+        SockWorld.loaded = true;
+        var e = document.getElementById('loadingMessage');
+        e.parentElement.removeChild(e);
+    }
+};
 
 var World = {
 
+
+    // ----------------------------------- POIS CODE ______________________________
 	cisRequestingData: false,
 	initiallyLoadedData: false,
 
@@ -30,11 +92,82 @@ var World = {
     pois: [],
     logs: [],
 
+    // ----------------------------------- GEO CODE ------------------------------------
+//    loaded: false,
+//    rotating: false,
+//
+//    init: function initFn() {
+//    this.createModelAtLocation();
+//    },
+//
+//    createModelAtLocation: function createModelAtLocationFn() {
+//
+//    /*
+//     First a location where the model should be displayed will be defined. This location will be relativ to the user.
+//     */
+//        var location = new AR.RelativeLocation(null, 5, 0, 2);
+//
+//    /*
+//     Next the model object is loaded.
+//     */
+//        var modelEarth = new AR.Model("assets/huntr-dirtysock.wt3", {
+//                                  onLoaded: this.worldLoaded,
+//                                  scale: {
+//                                  x: 2,
+//                                  y: 2,
+//                                  z: 2
+//                                  },
+//                                      rotate: {
+//                                      roll: 180.0,
+//                                      tilt: 0.0,
+//                                      heading: 0.0
+//                                      },
+//
+//                                      translate: {
+//                                      x: 5,
+//                                      y: 5,
+//                                      z: 0
+//                                      }
+//                                  });
+//
+//        var indicatorImage = new AR.ImageResource("assets/indi.png");
+//
+//        var indicatorDrawable = new AR.ImageDrawable(indicatorImage, 0.1, {
+//                                                 verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+//                                                 });
+//
+//    /*
+//     Putting it all together the location and 3D model is added to an AR.GeoObject.
+//     */
+//        var obj = new AR.GeoObject(location, {
+//                               drawables: {
+//                               cam: [modelEarth],
+//                               indicator: [indicatorDrawable]
+//                               }
+//                               });
+//    },
+//
+//    worldLoaded: function worldLoadedFn() {
+//        World.loaded = true;
+//        var e = document.getElementById('loadingMessage');
+//        e.parentElement.removeChild(e);
+//    },
+//
+    // ----------------------------------- END OF GEO CODE -------------------------------
+
     createMarker: function createMarkerFn(specificPoi) {
         World.markerDrawable_idle = new AR.ImageResource("assets/marker_idle.png");
         World.markerDrawable_selected = new AR.ImageResource("assets/marker_selected.png");
         return new Marker(specificPoi);
     },
+
+//    createTreasure: function createTreasureFn() {
+//        World.sock = new AR.imageResource("assets/dirtysock1.png");
+    // hard code a geolocation
+    // use the location to put it into a geoObject
+    //
+//
+//    }
 
     // recursive check for user proximity to marker
     checkIfUserIsNearMarker: function() {
@@ -54,7 +187,7 @@ var World = {
         }
 
         setTimeout(function() {
-                   console.log('setTimeout called');
+            console.log('setTimeout called');
             World.checkIfUserIsNearMarker();
         }, 3000);
     },
@@ -65,9 +198,20 @@ var World = {
         World.currentMarker.markerObject.destroy();
         World.currentMarker = null;
         World.markerList = [];
+//        World.sock = new AR.imageResource("assets/dirtysock1.png");
 
         if (World.pois.length === 0) {
-            return alert('Congratz!');
+            alert("You found the treasure!! Click 'OK' to redeem your prize! ");
+            SockWorld.init();
+//            var sockImage = new AR.imageDrawable(World.sock, 5, {
+//                zOrder: 0,
+//                opacity: 1.0
+//                });
+
+//            return sockImage;
+
+//            return
+
         }
 
         World.currentMarker = World.createMarker(World.pois.shift());
@@ -84,6 +228,7 @@ var World = {
         World.pois = [];
         var that = this;
 
+        // DONT FORGET TO CHANGE BACK TO 0
 		for (var currentPlaceNr = 0; currentPlaceNr < poiData.length ; currentPlaceNr++) {
 			var poi = {
 				"id": poiData[currentPlaceNr].id,
@@ -141,17 +286,22 @@ var World = {
         $("#poi-detail-title").html(marker.poiData.title);
         $("#poi-detail-description").html(marker.poiData.description);
 
-        // $("#poi-resolved").click(function() {
-        //     $("#panel-poidetail").panel("close");
-        //     marker.poiData.onClose();
-        // });
+        $("#poi-resolved").click(function() {
+            $("#panel-poidetail").panel("close");
+            marker.poiData.onClose();
+        });
+
+
+        var distanceToUserValue = (marker.distanceToUser > 999) ? ((marker.distanceToUser / 1000).toFixed(2) + " km") : (Math.round(marker.distanceToUser) + " m");
+
+        $("#poi-detail-distance").html(distanceToUserValue);
 
         $("#panel-poidetail").panel("open", 123);
 
         $(".ui-panel-dismiss").unbind("mousedown");
 
         $("#panel-poidetail").on("panelbeforeclose", function(event, ui) {
-            // $("#poi-resolved").unbind('click');
+            $("#poi-resolved").unbind('click');
             World.currentMarker.setDeselected(World.currentMarker);
         });
     },
@@ -181,6 +331,7 @@ var World = {
         });
 	}
 };
+//SockWorld.init();
 
 AR.context.onLocationChanged = World.locationChanged;
 
