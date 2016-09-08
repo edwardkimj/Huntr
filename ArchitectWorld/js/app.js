01c1939a9f16a2584e0f65a5a9a71ae1e525eddb
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-09-07 20:39:56 -0700

    Add list of maps to hunt index page and event listener on hunts

--- ArchitectWorld/js/app.js
+++ ArchitectWorld/js/app.js
@@ -1,37 +1,75 @@
-$('document').ready(function() {
+$(document).ready(function() {
 
   $("#panel-poidetail").panel("open", 123);
 
   console.log("I'm in the app.js file");
-  // $.ajaxSetup ({
-  //   cache: false
-  // });
+  $.ajaxSetup ({
+    cache: false
+  });
 
   function loadMapListPage() {
-    $('#main').load('../ArchitectWorld/map-list.html', function() {
-
-      function renderMaps(maps) {
-        $('#map-list').empty();
-        for( var i = 0; i < maps.length; i++ ) {
-          console.log("I'm in the for loop for getting maps");
-          $('#map-list').append(`<li><a href="#">${maps[i].name}</a></li>`);
-        }
+    function renderMaps(maps) {
+      $('#map-list').empty();
+      for( var i = 0; i < maps.length; i++ ) {
+        console.log("I'm in the for loop for getting maps");
+        $('#map-list').append(`<li><a  href="/#maps/${maps[i].id}">${maps[i].name}</a></li>`);
+        listenForClick();
       }
-      $.ajax({
-        url: "https://protected-anchorage-46542.herokuapp.com/users/2/games/",
-        dataType: "JSON",
-        type: "GET",
-        error: function(err) {
-          console.log("maps list request failed");
-        },
-        success: function(response) {
-          console.log("maps list request successful");
-          renderMaps(response);
-        }
-      })
+    }
+    $.ajax({
+      url: "https://protected-anchorage-46542.herokuapp.com/users/2/games/",
+      dataType: "JSON",
+      type: "GET",
+      error: function(err) {
+        console.log("maps list request failed");
+      },
+      success: function(response) {
+        console.log("maps list request successful");
+        renderMaps(response);
+      }
     })
+  };
+
+  function loadMapDetailPage(mapId) {
+    $('#main').load('../ArchitectWorld/map-details.html', function() {
+      console.log("I'm in the load map detail page function");
+    });
   }
 
-  loadMapListPage();
 
-})
+  function setView() {
+    console.log("I'm in setView!!!")
+    var hashParts = document.location.hash.split('/');
+    if (hashParts[0] === '') {
+      console.log("i'm in the setView and about to load the maplist page")
+      loadMapListPage();
+    } else {
+      console.log("i'm in setview and i'm trying to go to details")
+      loadMapDetailPage(hashParts[1]);
+    }
+  }
+
+  $('#map-list a').on('tap', function(event) {
+      event.preventDefault();
+      // grab href with the maps id
+      // clear out the html page and inject details of maps
+      console.log("clicked on map-quest link")
+  })
+
+  setView();
+
+  window.addEventListener('hashchange', setView);
+});
+
+
+function listenForClick() {
+  $('#map-list').on('click', 'a', function(event) {
+    event.preventDefault();
+    // grab href with the maps id
+    // clear out the html page and inject details of maps
+    console.log("clicked on map-quest link")
+  });
+};
+
+
+//if (hashParts[0] === '#maps' && hashParts[1]) {
